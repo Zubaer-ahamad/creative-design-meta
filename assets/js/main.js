6dfd39a5757f84a0cc3aa9aad4b3d4360c811220
Zubaer-ahamad <zunaedshibli26@gmail.com>
2024-04-25 17:01:57 +0600

    dark mode function re done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,11 @@ function toggleMenu() {
     document.getElementById('nav-icon').classList.toggle('active');
 }
 
-function toggleDark() {
-    const icon = document.getElementById('head-logo');
-    const adImage = document.getElementById('ad-img');
-    document.querySelector('body').classList.toggle('darkTheme');
+const icon = document.getElementById('head-logo');
+const adImage = document.getElementById('ad-img');
 
+function toggleDark() {
+    document.querySelector('body').classList.toggle('darkTheme');
     localStorage.setItem("darkMode", 'on');
 
     if (document.body.classList.contains('darkTheme')) {
@@ -22,4 +22,10 @@ function toggleDark() {
 
 if (localStorage.getItem('darkMode') === "on") {
     document.querySelector('body').classList.add("darkTheme");
+    icon.src = "assets/images/dark-logo.png";
+    adImage.src = "assets/images/dark-ads-space.png";
+} else {
+    icon.src = "assets/images/Logo.png";
+    adImage.src = "assets/images/ads-space.png";
+    localStorage.setItem("darkMode", "");
 }
