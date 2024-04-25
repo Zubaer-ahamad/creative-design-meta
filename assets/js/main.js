b2f82eeb88eea819ba5a5ba968b169c8a3b2fbde
Zubaer-ahamad <zunaedshibli26@gmail.com>
2024-04-25 15:40:08 +0600

    night mode home page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
-    document.getElementById('nav-icon').classList.toggle('active')
+    document.getElementById('nav-icon').classList.toggle('active');
 }
+
+function toggleDark() {
+    const icon = document.getElementById('head-logo')
+    document.querySelector('body').classList.toggle('darkTheme');
+    if (document.body.classList.contains('darkTheme')) {
+        icon.src = "assets/images/dark-logo.png"
+    }
+    else {
+        icon.src = "assets/images/Logo.png"
+    }
+} 
