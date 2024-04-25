fef814ea2219712b0eb92b41a18cd2698e900c28
Zubaer-ahamad <zunaedshibli26@gmail.com>
2024-04-25 16:41:44 +0600

    dark mode complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,23 @@ function toggleMenu() {
 }
 
 function toggleDark() {
-    const icon = document.getElementById('head-logo')
+    const icon = document.getElementById('head-logo');
+    const adImage = document.getElementById('ad-img');
     document.querySelector('body').classList.toggle('darkTheme');
+
+    localStorage.setItem("darkMode", 'on');
+
     if (document.body.classList.contains('darkTheme')) {
-        icon.src = "assets/images/dark-logo.png"
+        icon.src = "assets/images/dark-logo.png";
+        adImage.src = "assets/images/dark-ads-space.png";
     }
     else {
-        icon.src = "assets/images/Logo.png"
+        icon.src = "assets/images/Logo.png";
+        adImage.src = "assets/images/ads-space.png";
+        localStorage.setItem("darkMode", "");
     }
-} 
+}
+
+if (localStorage.getItem('darkMode') === "on") {
+    document.querySelector('body').classList.add("darkTheme");
+}
